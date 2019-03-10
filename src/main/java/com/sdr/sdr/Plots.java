b9b57a2bace904a540a5b49634b25fe37d67067e
Heladio Amaya <heladio.ac@gmail.com>
2019-03-10 12:56:20 -0600

    Le ponen un mensajito chido aquí sobre lo que agregaron

--- src/main/java/com/sdr/sdr/Plots.java
+++ src/main/java/com/sdr/sdr/Plots.java
@@ -4,5 +4,7 @@ import static com.sdr.sdr.Main.db;
 //Importar la base de datos para no realizar más conexiones
 
 public class Plots {
-    
+    public static void kiubo() {
+        System.out.println("Kiubo");
+    }
 }
