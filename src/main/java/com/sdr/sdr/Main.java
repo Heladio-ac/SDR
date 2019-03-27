9fbb935e00608113cb3966977f10a96a82bd5f17
Heladio Amaya <heladio.ac@gmail.com>
2019-03-27 14:43:36 -0600

    Refactorización para reducir conexiones Volver estáticos métodos de tabla

--- src/main/java/com/sdr/sdr/Main.java
+++ src/main/java/com/sdr/sdr/Main.java
@@ -2,6 +2,7 @@ package com.sdr.sdr;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import javax.swing.table.DefaultTableModel;
 
 public class Main {
     
@@ -9,6 +10,49 @@ public class Main {
     
     public static void main(String[] args) {
         MongoClient conexion =  new MongoClient();
-        db = conexion.getDatabase("MetalStorm");
+        db = conexion.getDatabase("SDR");
+        
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(INVENTARIO.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(INVENTARIO.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(INVENTARIO.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(INVENTARIO.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new INVENTARIO().setVisible(true);
+            }
+        });
     }
+    
+    public static DefaultTableModel tabla(){
+        String[] columnNames = {"Articulo", "Descripcion", "Cantidad"};
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0){
+            @Override
+            public boolean isCellEditable(int fil, int col) {
+                return col==3;
+            }
+        };
+        return model;
+    }
+    
 }
